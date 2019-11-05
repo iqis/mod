2f65b47ff26a148f32793463ae85180eea6b9745
Siqi Zhang <iqis.gnahz@gmail.com>
2019-11-05 16:00:04 -0500

    ...

--- R/utils.R
+++ R/utils.R
@@ -27,39 +27,39 @@ as_module <- function(package){
 
 masks <- new.env(parent = emptyenv())
 
-masks$library <- function(){
+masks$library <- function(...){
         stop("Only use library() outside of a module;
              To load and attach a package locally, use require().")
 }
 
-masks$attach <- function(){
+masks$attach <- function(...){
         stop("Only use attach() outside of a module;
              To copy objects from another module, use refer();
              To load and attach a package locally, use require().")
 }
 
-masks$detach <- function(){
+masks$detach <- function(...){
         stop("Only use detach() outside of a module.")
 }
 
-masks$install.packages <- function(){
+masks$install.packages <- function(...){
         stop("Only use install.packages() outside of a module.")
 }
 
-masks$update.packages <- function(){
+masks$update.packages <- function(...){
         stop("Only use update.packages() outside of a module.")
 }
 
-masks$use <- function(){
+masks$use <- function(...){
         stop("Only use use() outside of a module;
              To copy objects from another module, use refer().")
 }
 
-masks$drop <- function(){
+masks$drop <- function(...){
         stop("Only use drop() outside of a module;")
 }
 
-masks$source <- function(){
+masks$source <- function(...){
         stop("Only use source() outside of a module;
              To make a nested module, use module()/acquire().")
 }
