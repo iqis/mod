3b60c270d06023bce371504dfe055b459a15718c
Siqi Zhang <iqis.gnahz@gmail.com>
2019-12-04 11:23:51 -0500

    remove `thing()` and related.

--- tests/testthat/test-thing.R
+++
@@ -1,8 +0,0 @@
-thing_test <- mod::thing({
-        a <- 1
-}, dot = function()a)
-
-test_that("thing() and its active binding works", {
-        expect_true(mod::is_thing(thing_test))
-        expect_equal(thing_test$., thing_test$a, thing_test[])
-})
